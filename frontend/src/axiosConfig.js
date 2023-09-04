44091161c477b9b82794d8f415c1dad4319c2287
Suraj Giri <sg746592@gmail.com>
2023-09-04 12:04:07 +0545

    updated2

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,5 +3,5 @@ import axios from "axios";
 axios.defaults.baseURL =
   // process.env.NODE_ENV !== "production" ? "http://localhost:5000" : "/";
   process.env.NODE_ENV !== "production"
-    ? "https://foodaajareactnode.onrender.com"
+    ? "https://foodaajareactapp.onrender.com"
     : "/";
